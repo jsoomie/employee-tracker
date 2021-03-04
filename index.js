0d836745f6cabad8491ad627122a7824810dae04
Jonathan Soukaseume <sksmejn@gmail.com>
2021-03-03 16:22:47 -0800

    removed nodemone

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const mysqlUser = "Jon";
 const mysqlPw = "123";
 const database = "company";
 
-// Connects to the database
+// Creates connection to the specified database at the sepcified port
 const db = mysql.createConnection({
     host: "localhost",
     port: PORT,
@@ -22,11 +22,17 @@ const linebreak = (symbol = "-", repeatTime = 35) => console.log(`\n${symbol.rep
 
 // Starts the questions
 const start = () => {
+    // A little welcome box
+    linebreak("=", 50);
+    console.log("=== Welcome to the employee management system! ===");
+    linebreak("=", 50);
+
+    // Prompts the user
     inquirer.prompt(
         {
             type: "list",
             name: "start",
-            message: "What would you like to do?",
+            message: "How would you like to start?",
             choices: [
                 "Employee Management",
                 "EXIT"
@@ -43,6 +49,9 @@ const start = () => {
                 console.log("Bye!");
                 db.end();
                 break;
+            default: 
+                console.log("Something went wrong...ending connection...");
+                db.end();
         }
     })
 }
@@ -50,18 +59,108 @@ const start = () => {
 // Starts the management process of employees
 const manage = () => {
     console.log("Starting 'manage' function!");
-}
 
-// Starts a connection on a port
+    // array of choices here
+    const choices = [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        new inquirer.Separator(),
+        "Add Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "Remove Employee",
+        new inquirer.Separator(),
+        "View All Roles",
+        "Add Role",
+        "Remove Role",
+        new inquirer.Separator(),
+        "View All Departments",
+        "Add Department",
+        "Remove Department",
+        new inquirer.Separator(),
+        "EXIT"
+    ]
+
+    // Array of questions here
+    const question = [
+        {
+            type: "list",
+            name: "manageList",
+            message: "What would you like to do?",
+            choices: choices,
+        }
+    ];
+
+    inquirer.prompt(question).then((answers) => {
+        switch(answers.manageList) {
+            case "View All Employees":
+                return viewEmployees();
+            case "View All Employees By Department":
+                return viewEmployeesDept();
+            case "View All Employees By Manager":
+                return viewEmployeesManager();
+            default:
+                db.end();
+        };
+    })
+};
+
+const viewEmployees = () => {
+    console.log("View All Employees");
+
+    db.query("SELECT ")
+};
+
+const viewEmployeesDept = () => {
+    console.log("View All Employees Sort by Department");
+};
+
+const viewEmployeesManager = () => {
+    console.log("View All Employees Sort By Manager");
+};
+
+const addEmployee = () => {
+    console.log("Add an Employee");
+};
+
+const updateEmployeeRole = () => {
+    console.log("Update Employee's Role");
+};
+
+const updateEmployeeManager = () => {
+    console.log("Update Employee's Manager");
+};
+
+const viewDept = () => {
+    console.log("Viewing All Department");
+};
+
+const addDept = () => {
+    console.log("Add a department")
+};
+
+const removeDept = () => {
+    console.log("Remove a department");
+};
+
+const viewRoles = () => {
+    console.log("Viewing all roles");
+};
+
+const addRole = () => {
+    console.log("Adding a role");
+};
+
+const removeRole = () => {
+    console.log("Remove a role");
+};
+
+// Starts a connection
 db.connect((err) => {
     if(err) throw err;
     console.log(`Listening on port: ${PORT}`);
 
-    // A little welcome box
-    linebreak("=", 50);
-    console.log("=== Welcome to the employee management system! ===");
-    linebreak("=", 50);
-
     // Runs the program
     start();
-})
+});
